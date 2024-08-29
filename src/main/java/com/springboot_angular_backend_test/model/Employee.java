03b87af4e8a71a60ba2e6fabc3bde94c764089de
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-08-29 17:03:41 -0300

    Classe de entidade criada

--- src/main/java/com/springboot_angular_backend_test/model/Employee.java
+++ src/main/java/com/springboot_angular_backend_test/model/Employee.java
@@ -1,4 +1,90 @@
 package com.springboot_angular_backend_test.model;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    @Column(name = "number_nis")
+    private Long numberNis;
+
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String firstName, String lastName, String emailId, Long numberNis) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+        this.numberNis = numberNis;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Long getNumberNis() {
+        return numberNis;
+    }
+
+    public void setNumberNis(Long numberNis) {
+        this.numberNis = numberNis;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
