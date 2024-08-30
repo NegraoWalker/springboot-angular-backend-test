4a26cebdb0c7ae0a20af3c98cf568335f3830aeb
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-08-30 07:20:01 -0300

    Validação do campo nnumberNis realizada

--- src/main/java/com/springboot_angular_backend_test/dto/EmployeeDto.java
+++ src/main/java/com/springboot_angular_backend_test/dto/EmployeeDto.java
@@ -1,7 +1,6 @@
 package com.springboot_angular_backend_test.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.Objects;
 
@@ -13,6 +12,7 @@ public class EmployeeDto {
     private String lastName;
     @Email(message = "O e-mail deve ser válido!", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     private String emailId;
+    @Digits(integer = 11, message = "O número NIS deve ter 11 digitos", fraction = 0)
     private Long numberNis;
 
     public EmployeeDto() {
